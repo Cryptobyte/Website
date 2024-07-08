dfb0e2ccb22a29dfeb64fec94da3b6331cd5d885
Cryptobyte <me@cryptobyte.dev>
2024-07-07 22:40:44 -0400

    Update new slider

--- src/Components/PortfolioComponent.tsx
+++ src/Components/PortfolioComponent.tsx
@@ -6,17 +6,17 @@ const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 3,
-    slidesToSlide: 3 // optional, default to 1.
+    slidesToSlide: 3
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 2,
-    slidesToSlide: 2 // optional, default to 1.
+    slidesToSlide: 2
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1 // optional, default to 1.
+    slidesToSlide: 1
   }
 };
 
@@ -70,17 +70,19 @@ export default function PortfolioComponent() {
           <Carousel
             swipeable={false}
             draggable={false}
-            showDots={true}
+            showDots={false}
             responsive={responsive}
             infinite={true}
             autoPlaySpeed={1000}
-            keyBoardControl={true}
+            keyBoardControl={false}
             customTransition="all .5"
             transitionDuration={500}
             containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
             // deviceType={this.props.deviceType}
-            dotListClass="custom-dot-list-style"
-            itemClass="carousel-item-padding-40-px">
+            // dotListClass="custom-dot-list-style"
+            // itemClass="carousel-item-padding-40-px"
+            >
 
             {!loading && resume.map((project: any, index) => (
               <div key={index} className="item">
