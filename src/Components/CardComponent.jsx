59e9c22860e3bd2e9da47e6276823eae946ed3ca
Cryptobyte <me@cryptobyte.dev>
2024-07-08 02:01:47 -0400

    When in doubt, switch back to JS

--- src/Components/CardComponent.jsx
+++ src/Components/CardComponent.jsx
@@ -12,22 +12,7 @@ import {
 
 import mapIcon from '../Utilities/iconMapper';
 
-interface links {
-  href: string
-  title: string
-}
-
-interface CardComponentProps {
-  slug: string
-  name: string
-  year: string
-  tags: string[]
-  links: links[]
-  image: string
-  description: string
-}
-
-export default function CardComponent({ slug, name, year, tags, links, image, description }: CardComponentProps) {
+export default function CardComponent({ slug, name, year, tags, links, image, description }) {
   return (
     <CCard textBgColor={'light'}>
       <CCardImage orientation="top" src={image} />
@@ -46,10 +31,10 @@ export default function CardComponent({ slug, name, year, tags, links, image, de
       </CCardBody>
 
       <CCardFooter>
-        {tags.map((tag, index): any => {
+        {tags.map((tag, index) => {
           const Icon = mapIcon(tag);
 
-          return Icon ? Icon : (<></>);
+          return Icon ? <Icon /> : (<></>);
         })}
       </CCardFooter>
     </CCard>
